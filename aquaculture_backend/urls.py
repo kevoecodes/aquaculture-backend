4cd515916d722bbc2d7ea8482b55d7c0d82123d2
kevoecodes <kelvinremmy@gmail.com>
2024-08-08 14:59:04 +0300

    Commit

--- aquaculture_backend/urls.py
+++ aquaculture_backend/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/v1/devices', DevicesView.as_view()),
     path('api/v1/user-from-token', GetUserFromTokenView.as_view()),
     path('api/v1/switch-motor', SwitchView.as_view()),
+
 ]
